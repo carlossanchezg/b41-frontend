8327d50dba0e9a6fae08059a0996cd80ec593f2d
Carlos Sanchez <carlossanchez@MacBook-Pro-de-Carlos.local>
2020-06-15 18:37:08 -0600

    agregar ejemplo de uso de proveedor para encapsular a los consumidpores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Navigation from './Components/Navigation';
+import Prueba from './Components/Prueba'
+import AuthContextProvider from './contexts/AuthContext'
 
 function App() {
   return (
     <React.Fragment>
-      <Navigation />
-      <h1>Hola Mundo</h1>
+      <AuthContextProvider>
+        <Navigation />
+        <h1>Hola Mundo</h1>
+        <Prueba />
+      </AuthContextProvider>
     </React.Fragment>
   );
 }
